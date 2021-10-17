e7738695b1edcde0c12489de52f733cdc3107832
thatyppi <thatyppi@users.noreply.github.com>
2021-10-17 15:43:24 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.5343b811dbb10ad3f3dc23a79209a110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcfd6253d958d59cd7eb0930d2fc7b5",
+    "url": "https://perlia.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "16ba700600da9bc24572",
+    "url": "https://perlia.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0d7e7ec037735577ac21",
+    "url": "https://perlia.github.io/task-management-frontend/static/js/2.03d1f558.chunk.js"
+  },
+  {
+    "revision": "16ba700600da9bc24572",
+    "url": "https://perlia.github.io/task-management-frontend/static/js/main.0f1be8d5.chunk.js"
+  },
+  {
+    "revision": "85be29d576bc21f172cc",
+    "url": "https://perlia.github.io/task-management-frontend/static/js/runtime~main.6a051af6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://perlia.github.io/task-management-frontend/precache-manifest.5343b811dbb10ad3f3dc23a79209a110.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://perlia.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
